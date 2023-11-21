c1dd56082b8b111c5eb78599c11368d39aee3c77
Rajan Sharma <82056580+Immortal-codes@users.noreply.github.com>
2023-11-21 13:07:48 +0530

    Add files via upload
    just code files , code files of frontend and MongoDB connectivity file

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+//const moment = require("moment");
+
+const app = express();
+const port = 8000;
+const cors = require("cors");
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose
+  .connect("mongodb+srv://rajanr8979:Rajan8979@cluster0.hjxblia.mongodb.net/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB", error);
+  });
+
+app.listen(port, () => {
+  console.log("Server is running on port 8000");
+});
+
